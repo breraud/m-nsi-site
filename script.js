0456a1c6faccac719a5fc3912a9c28f655de7bbd
findel2207 <40869248+findel2207@users.noreply.github.com>
2022-02-09 14:15:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changeData(newURL) {
 
-    if(window.screen.width > 991){
+    if(window.screen.width > 880){
         var adobeDCView = new AdobeDC.View({clientId: "5486c130612343e9a097b73035401f0f", divId: "viewer"});
         adobeDCView.previewFile({
           content:{ location:
