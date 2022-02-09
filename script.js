2bc16d2198e7133e8b890cf499c69da9c4d44002
Sn0wx <40869248+findel2207@users.noreply.github.com>
2022-02-09 22:35:41 +0100

    changing orientation problem

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 var lastURL = "https://findel2207.github.io/pdf/msec/rien.pdf";
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function checkMenu(){
+    let btn = document.getElementById("menu-btn");
+    if(isIpad() && isLandscape()){
+        btn.classList.remove("hidden");
+    }
+    else if(btn.classList.contains("hidden")){
+        return;
+    }
+    else{
+        btn.classList.add("hidden");
+    }
+
+}
+
+window.onload = (event) => {
+    checkMenu();
+};
+
+window.addEventListener("orientationchange", function() {
+    sleep(2000);
+    checkMenu();
+});
+
 document.addEventListener("adobe_dc_view_sdk.ready", function()
 {
     var adobeDCView = new AdobeDC.View({clientId: "5486c130612343e9a097b73035401f0f", divId: "viewer"});
