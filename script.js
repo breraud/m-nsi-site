0a131dcd8323250a4614b0f9c2d50c4094776831
Sn0wx <40869248+findel2207@users.noreply.github.com>
2022-02-09 20:10:36 +0100

    corriging lot of bugs, fixing slider problem

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var lastURL = "https://findel2207.github.io/pdf/msec/rien.pdf";
+
+document.addEventListener("adobe_dc_view_sdk.ready", function()
+{
+    var adobeDCView = new AdobeDC.View({clientId: "5486c130612343e9a097b73035401f0f", divId: "viewer"});
+    adobeDCView.previewFile(
+   {
+      content:   {location: {url: "https://findel2207.github.io/pdf/msec/rien.pdf"}},
+      metaData: {fileName: "Accueil"}
+   });
+});
+
 function isIpad(){
     const iPad = (navigator.userAgent.match(/(iPad)/) /* iOS pre 13 */ || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) /* iPad OS 13 */);
     const userAgent = navigator.userAgent.toLowerCase();
@@ -21,14 +33,17 @@ function isLandscape(){
     }
     
 
-function changeData(newURL, fileName) {
-    
+function changeData(newURL, filename) {
+    if(lastURL === newURL){
+        openURL(newURL)
+    }
     if(window.screen.width > 991 || (isIpad() && isLandscape())){
+        lastURL = newURL;
         var adobeDCView = new AdobeDC.View({clientId: "5486c130612343e9a097b73035401f0f", divId: "viewer"});
         adobeDCView.previewFile({
           content:{ location:
             { url: newURL}},
-          metaData:{fileName: fileName}
+          metaData:{fileName: filename}
         },
         {
           embedMode: "SIZED_CONTAINER"
@@ -42,65 +57,3 @@ function changeData(newURL, fileName) {
 function openURL(URL){
     window.open(URL, '_blank');
 }
-
-
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;                                                        
-var yDown = null;
-var swiped = false
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-                                                                         
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-                                                                         
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-                                                                         
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* right swipe */
-            if(swiped === false){
-                swiped = true
-                document.getElementById("menu").style.width = '0px';
-                document.getElementById("viewer").style.width = '100vw';
-                document.getElementById("viewer").style.position = 'absolute';
-                document.getElementById("viewer").style.left = '0px';
-            }
-        } else {
-            /* left swipe */
-            if(swiped === true){
-                swiped = false
-                document.getElementById("menu").style.width = '280px';
-                document.getElementById("viewer").style.width = 'calc(100vw - 280px)';
-                document.getElementById("viewer").style.position = 'relative';
-                /*document.getElementById("viewer").style.left = '0px';*/
-            }
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* down swipe */ 
-        } else { 
-            /* up swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
